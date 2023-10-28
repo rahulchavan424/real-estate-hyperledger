5f2ac39f5a8e91a49a1c8452c9d8d3067e2f88c0
rutu-droid <rutu54223@gmail.com>
2023-10-29 00:36:41 +0530

    test:code for the frontend

--- application/client/src/App.jsx
+++ application/client/src/App.jsx
@@ -1,12 +1,15 @@
-import { useState } from 'react'
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-    </>
-  )
-}
+    <div id="app">
+      <Router>
+        <Route path="/" component={MainView} />
+      </Router>
+    </div>
+  );
+};
 
-export default App
+export default App;
